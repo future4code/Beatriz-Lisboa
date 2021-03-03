05567d92bab1a9f354f2ff1d9581449d6899303c
Beatriz Moura <lmourabia@gmail.com>
2021-03-03 16:28:29 -0300

    Edição do index.js

---
+++ semana4/objetos/blog/blog-boilerplate/index.js
@@ -0,0 +1,27 @@
+
+const pressButton = () => {
+const button = document.getElementById("post-container") {
+
+}
+
+const formulario = document.getElementById("form-user") {
+
+}
+
+const tituloPost = document.getElementById("post-tittle") {
+const
+}
+
+const tituloAutor = document.getElementById("autor-tittle") {
+
+}
+
+const conteudoPost = document.getElementById("post-text") {
+
+}
+
+
+
+
+
+}
