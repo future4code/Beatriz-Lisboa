b71cc69f75031f4bf5cdccda13f8eee25eaa2dd9
Beatriz Moura <lmourabia@gmail.com>
2021-03-03 18:55:09 -0300

    alteração final do index.html e index.js

--- semana4/objetos/blog/blog-boilerplate/index.js
+++ semana4/objetos/blog/blog-boilerplate/index.js
@@ -1,27 +1,30 @@
 
-const pressButton = () => {
-const button = document.getElementById("post-container") {
+const pressButton = () => { 
+
+const formUser = document.getElementById("formulario") 
+
+const postTittle = document.getElementById("titulo-post") 
+
+const autorTittle = document.getElementById("autor-post") 
+
+const postText = document.getElementById("conteudo-post")
+
+const post = {
+tittle: postTittle.value, 
+autor: autorTittle.value,
+text: postText.value  
 
 }
+let arrayObjetos = []
+arrayObjetos.push(post)
+console.log(arrayObjetos)
+postTittle.value =""
+autorTittle.value =""
+postText.value =""
 
-const formulario = document.getElementById("form-user") {
-
-}
-
-const tituloPost = document.getElementById("post-tittle") {
-const
-}
-
-const tituloAutor = document.getElementById("autor-tittle") {
-
-}
-
-const conteudoPost = document.getElementById("post-text") {
-
-}
-
-
-
-
+const container = document.getElementById("container-de-posts")
+container.innerHTML += "<h2>"+post.tittle +"</h2>" 
+container.innerHTML += "<h3>"+post.autor+"</h3>"
+container.innerHTML += "<p>"+post.text+"</p>"
 
 }
