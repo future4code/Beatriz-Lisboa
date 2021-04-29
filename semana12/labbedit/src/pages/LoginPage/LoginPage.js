cf4aa9a552a0778329d19e37a8b283a631e80a13
Beatriz Lisboa <lmourabia@gmail.com>
2021-04-29 20:25:44 -0300

    aplicação das constants color, url e theme

--- semana12/labbedit/src/pages/LoginPage/LoginPage.js
+++ semana12/labbedit/src/pages/LoginPage/LoginPage.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import {Button} from "@material-ui/core"
+import { ScreenContainer } from './styled'
 
 const LoginPage = () => {
     return (
-        <div>
+        <ScreenContainer>
             <h1>LoginPage</h1>
-        </div>
+            <Button variant="contained" color="primary">
+              Entrar
+            </Button>
+            <h1>Cadastro</h1>
+            <Button variant="contained" color="primary">
+            Cadastrar</Button>
+        </ScreenContainer>
     )
 }
 
